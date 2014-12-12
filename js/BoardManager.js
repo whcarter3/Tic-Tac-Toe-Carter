5969ab43876c8113621b67971c48f1a2994e8e41
Will Carter <whcarter3@gmail.com>
2014-12-11 19:27:53 -0800

    Added X & O when players click on square and disallows clicking on the same square

--- js/BoardManager.js
+++ js/BoardManager.js
@@ -9,27 +9,37 @@ function BoardManager (PlayerManager, numSquares) {
 
 	var board = function  (numSquares) {
 		
-		this.numSquares 	= numSquares;
+		// this.numSquares 	= numSquares;
 		this.squares 		= new Array( numSquares );
-		this.toggleState 	= toggleState;
+		this.switchTurn 	= switchTurn;
 		this.getSquareState = getSquareState;
 		this.clearBoard 	= clearBoard;
 
 		this.clearBoard();
 
-		//switches between 0, 1, 2 in the array
-		function toggleState (num) {
-			this.squares[num] = (this.squares[num] + 1) % SQUARE_STATE.length;
+		//switches between an index of 0, 1, 2 in the array
+		var turnCounter = 1;
+		function switchTurn (num) {
+			// this.squares[num] = (this.squares[num] + 1) % SQUARE_STATE.length;
+			if (this.squares[num] == 0) {	
+				if (turnCounter == 1) {
+					this.squares[num] = "X";
+					turnCounter = 2;
+				} else {
+					this.squares[num] = "O";
+					turnCounter = 1;
+				}
+			}
 		}
 
-		//gets the array SQUARE_STATE and sets it to 0
+		//switches the index number of SQUARE STATE in order to flip through each class
 		function getSquareState (num) {
 			return SQUARE_STATE[this.squares[num]];
 		}
-		//sets each of the state arrays to 0
+		//sets the index number of the state of the square array to 0
 		function clearBoard() {
 			for (var i = 0; i < this.squares.length; i++) {
-				this.squares[i] = 0;
+				this.squares[i] = "";
 			};
 		}
 	};
